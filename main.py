f563c16054deb4f88b97ae72c26444d605dd3963
Al-Hassan Abdel-Raouf <alhassanaraouf@disroot.org>
2020-03-16 17:48:24 +0200

    Update The Notes and Added Sentiment Analysis Code to The Project
    Note Now Describe The Current State of Project and Added Some Note
    That Might Help in Future

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from Sentiment.Database import Client
 from Sentiment.TextProcessing import Cleaning
 from Sentiment.TextProcessing import Translation
 from Sentiment.API import TwitterApi
+from Sentiment.Sentiment import polarity_scores
+
 test = Client('radwa', '123456as')
 test.connect()
 print(test.tdata()[0])
@@ -18,3 +20,5 @@ print(test.translate("שמי אל-חסן אשיש", 'en'))
 twitter = TwitterApi()
 print(twitter.Search('mi', 3, 'en'))
 print(twitter.AccountTweets('qabeeljr', 1))
+
+print(polarity_scores("good"))
