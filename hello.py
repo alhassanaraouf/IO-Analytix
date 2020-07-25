b8d7c28ceca1e60bdcbd4956cb1c3fbbe3c81f93
Al-Hassan Abdel-Raouf <alhassanaraouf@disroot.org>
2020-07-25 21:52:05 +0200

    fix 404 and 500 pages

--- hello.py
+++ hello.py
@@ -117,12 +117,12 @@ class Key2Form(FlaskForm):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"), 404
+    return render_template("404.html", base=base), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template("500.html"), 500
+    return render_template("500.html", base=base), 500
 
 
 def makedatalist(key, realtime=True):
